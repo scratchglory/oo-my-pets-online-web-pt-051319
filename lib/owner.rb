5acd61743e84c0805586b60e88931ede75084dfc
Adella <adellakenn@gmail.com>
2019-06-12 22:02:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
   
   
   def list_pets
-   "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s) ."
+   "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
   
   
