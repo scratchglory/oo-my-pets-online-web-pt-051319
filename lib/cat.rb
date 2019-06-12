be605b47cb605f0dd8b81a925475dd0a6bc086a3
Adella <adellakenn@gmail.com>
2019-06-12 22:02:53 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
+=begin
+  Cat
+  can initialize a cat (FAILED - 1)
+  initializes with a name (FAILED - 2)
+  can't change its name (FAILED - 3)
+  initializes with a nervous mood (FAILED - 4)
+  can change its mood (FAILED - 5)
+=end
+
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name 
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
